a68c64e9c2ea393c99ab0071debaed3d6ba564ae
Marcos Couto <marcoscouto1997@gmail.com>
2023-01-15 19:53:43 -0300

    versão com requisito imagens dos parrots

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 let perguntaNumCartas = prompt("Com quantas cartas quer jogar?");
 
+
+// pergunta pro usuário com quantas cartas quer jogar
 function verificaNumCartas() {
 
     while (perguntaNumCartas%2 == 1 || perguntaNumCartas < 4 || perguntaNumCartas > 14) {
@@ -10,22 +12,18 @@ function verificaNumCartas() {
 verificaNumCartas();
 
 
-//criar uma array pra colocar as cartas
-// trazer a lista do html pro js
-// alterar de acordo com o numero de cartas
-// embaralhar as cartas
-// mandar elas de volta pro html
 
-
-let numCartas = [];
+let numCartas = []; //array que vai receber as cartas
 
 function comparador() { 
 	return Math.random() - 0.5; 
-}
+} // função que embaralha as cartas
 
 const gif = ['bobross','bobross','explody','explody','fiesta','fiesta','metal','metal',
-'revertit','revertit', 'triplets','triplets', 'unicorn', 'unicorn'];
+'revertit','revertit','triplets','triplets','unicorn'];
 
+
+// vai adicionar cartas no DOM de forma dinâmica, de acordo com a qtde que o usuário escolheu
 function adicionaCartaDOM() {
 
     const areaJogo = document.querySelector('.area-jogo');
@@ -34,22 +32,21 @@ function adicionaCartaDOM() {
         let carta = `     
             <div class="carta">
                 <div class="frente-carta">
+                    <img src="./img/${gif[i]}parrot.gif" alt="gif"/>
                 </div>
                 <div class="costas-carta">
                     <img src="./img/back.png" alt="img parrot"/>
                 </div>
             </div>
         `
-        numCartas.push(carta);
+        numCartas.push(carta); // adiciona as cartas na array
     }
 
-    numCartas.sort(comparador);
+    numCartas.sort(comparador); //embaralha as cartas
 
     for (let j = 0; j < numCartas.length; j++) {
         areaJogo.innerHTML += numCartas[j];
-    }
+    } // adiciona cada elemento dessa array no DOM
 }
 
 adicionaCartaDOM();
-
-console.log(numCartas);
